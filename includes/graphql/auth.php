1da5fabaa3597c1b1f545a1f3df22d9c56a54f5a
kanji8210 <denisdekemet@gmail.com>
2026-05-12 20:16:04 +0300

    graph ql

--- includes/graphql/auth.php
+++ includes/graphql/auth.php
@@ -34,7 +34,34 @@ function construction_mgmt_get_authorization_header() {
     return '';
 }
 
-add_action('graphql_init', function() {
+function construction_mgmt_extract_jwt_user_id($decoded) {
+    if (is_object($decoded)) {
+        if (isset($decoded->user_id) && (int) $decoded->user_id > 0) {
+            return (int) $decoded->user_id;
+        }
+
+        if (isset($decoded->sub) && is_numeric($decoded->sub) && (int) $decoded->sub > 0) {
+            return (int) $decoded->sub;
+        }
+
+        if (isset($decoded->data) && is_object($decoded->data)) {
+            if (isset($decoded->data->user) && is_object($decoded->data->user)) {
+                if (isset($decoded->data->user->id) && (int) $decoded->data->user->id > 0) {
+                    return (int) $decoded->data->user->id;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+function construction_mgmt_apply_graphql_jwt_auth() {
+    static $already_processed = false;
+    if ($already_processed) {
+        return;
+    }
+
     $authorization = construction_mgmt_get_authorization_header();
     if ($authorization === '') {
         return;
@@ -52,10 +79,15 @@ add_action('graphql_init', function() {
         }
 
         $decoded = JWT::decode($token, new Key($secret, 'HS256'));
-        if (isset($decoded->user_id)) {
-            wp_set_current_user((int) $decoded->user_id);
+        $user_id = construction_mgmt_extract_jwt_user_id($decoded);
+        if ($user_id > 0) {
+            wp_set_current_user($user_id);
+            $already_processed = true;
         }
     } catch (Exception $e) {
         // Invalid token; leave request unauthenticated.
     }
-});
+}
+
+add_action('init', 'construction_mgmt_apply_graphql_jwt_auth', 1);
+add_action('graphql_init', 'construction_mgmt_apply_graphql_jwt_auth', 1);
